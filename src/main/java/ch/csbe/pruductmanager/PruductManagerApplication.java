fbb67d088817f0372b83cbf2dc857bed53d8ea2d
Aven <semrau.j@web.de>
2024-09-23 18:17:05 +0200

    Initial commit

---
+++ src/main/java/ch/csbe/pruductmanager/PruductManagerApplication.java
@@ -0,0 +1,13 @@
+package ch.csbe.pruductmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruductManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruductManagerApplication.class, args);
+    }
+
+}
