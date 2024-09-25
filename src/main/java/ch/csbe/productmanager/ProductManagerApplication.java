3e637112f2a4694524d5b5a46d3d366f0f19713f
Aven <semrau.j@web.de>
2024-09-25 22:52:05 +0200

    Delete playground

--- src/main/java/ch/csbe/productmanager/ProductManagerApplication.java
+++ src/main/java/ch/csbe/productmanager/ProductManagerApplication.java
@@ -2,9 +2,6 @@ package ch.csbe.productmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ProductManagerApplication {
@@ -13,14 +10,4 @@ public class ProductManagerApplication {
         SpringApplication.run(ProductManagerApplication.class, args);
     }
 
-    @RestController
-    @RequestMapping("playground")
-
-    public static class PlaygroundController {
-
-        @GetMapping("hello-world")
-        public String helloWorld() {
-            return "Hello Java Spring Boot";
-        }
-    }
 }
