07e6dc7e4b698261e45ff36506038ec17c752eee
Aven <semrau.j@web.de>
2024-09-25 20:51:36 +0200

    Delete non required Repository annotation on JPA repository

--- src/main/java/ch/csbe/productmanager/resources/product/ProductRepository.java
+++ src/main/java/ch/csbe/productmanager/resources/product/ProductRepository.java
@@ -1,12 +1,10 @@
 package ch.csbe.productmanager.resources.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 /**
  * Repository für den Zugriff auf die Produkte in der Datenbank.
  */
-@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
 }
